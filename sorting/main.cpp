b42b5553b69f77e03af74d41513a6fd5e89f5671
anas18x <anasshakil00@gmail.com>
2026-08-30 20:56:14 +0530

    diff sorting algos

--- sorting/main.cpp
+++ sorting/main.cpp
@@ -1,74 +1,58 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-
-// void bubbleSort(int arr[],int n){
-//   for(int i=0; i<n-1; i++){
-//     for(int j=0 ; j<n-i; j++){
-//         if(arr[j] > arr[j+1])
-//         swap(arr[j],arr[j+1]);
-//     }
-//   }  
-// }
-
-
-// ---- OPTIMISED
-void bubbleSort(int arr[],int n){
-  for(int i=0; i<n-1; i++){
+void bubbleSort(vector<int>& arr){
+  for (int i = 0; i < arr.size() - 1; i++){
     bool swapped = false;
 
-    for(int j=0 ; j<n-i; j++){
-        if(arr[j] > arr[j+1])
-        swap(arr[j],arr[j+1]);
-        swapped = true;
+    for (int j = 0; j < arr.size() - i - 1; j++) {
+      if (arr[j] > arr[j + 1])
+        swap(arr[j], arr[j + 1]);
+      swapped = true;
     }
-    if(swapped == false) break;
-  }  
+    if (swapped == false) break;
+  }
 }
 
 
 
 
-void insertionSort(int arr[], int n){
- for(int i=1; i<n; i++){                               //(assuming 1st element is sorted)
- int temp = arr[i];
-    int j = i-1;
+void insertionSort(vector<int>& arr){
+  for (int i = 1; i < arr.size(); i++){
+    int j = i;
 
-    for(; j>=0; j--){
-        if(arr[j] > temp){
-          // shift
-          arr[j+1] = arr[j];
-        }
-        else break;
+    while (j > 0 && arr[j - 1] > arr[j]){
+      swap(arr[j - 1], arr[j]);
+      j--;
     }
-    arr[j+1] = temp;
- }
+  }
 }
 
 
 
-void selectionSort(int arr[],int n){
- for(int i=0; i<n-1; i++){
+void selectionSort(vector<int>& arr){
+  for (int i = 0; i < arr.size() - 1; i++){
     int minValue = i;
-    for(int j=i+1; j<n; j++){
-        if(arr[j] < arr[minValue])
-          minValue = j;
+    for (int j = i + 1; j < arr.size(); j++){
+      if (arr[j] < arr[minValue])
+        minValue = j;
     }
-    swap(arr[i] , arr[minValue]);
- }
+    swap(arr[i], arr[minValue]);
+  }
 }
 
 
 
 int main(){
-int arr[] = {64,25,12,22,11};
-selectionSort(arr,sizeof(arr)/sizeof(arr[0]));
-insertionSort(arr,sizeof(arr)/sizeof(arr[0]));
-bubbleSort(arr,sizeof(arr)/sizeof(arr[0]));
+  vector<int> arr = {64, 25, 12, 22, 11};
+  selectionSort(arr);
+  insertionSort(arr);
+  bubbleSort(arr);
 
- for(auto value: arr){
-    cout<<value<<" ";
- }
-    return 0; 
+  for (auto value : arr)
+  {
+    cout << value << " ";
+  }
+  return 0;
 }
---
+++ sorting/mergeSort.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Function to merge two halves of the array
+void merge(vector<int>& arr, int low, int mid, int high) {
+    
+        // Create temp arrays
+        vector<int> temp;
+        int left = low, right = mid + 1;
+
+        // Merge two sorted halves
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right])
+                temp.push_back(arr[left++]);
+            else
+                temp.push_back(arr[right++]);
+        }
+
+        // Copy remaining elements from left half
+        while(left <= mid)
+            temp.push_back(arr[left++]);
+
+        // Copy remaining elements from right half
+        while(right <= high)
+            temp.push_back(arr[right++]);
+
+        // Copy sorted elements back to original array
+        for(int i = low; i <= high; i++)
+            arr[i] = temp[i - low];
+    }
+
+
+
+void mergeSort(vector<int>& arr, int low, int high) {
+
+        if (low >= high) return;
+
+        int mid = (low + high) / 2;
+
+        // Recursively sort left half
+        mergeSort(arr, low, mid);
+
+        // Recursively sort right half
+        mergeSort(arr, mid + 1, high);
+
+        // Merge the two sorted halves
+        merge(arr, low, mid, high);
+}
+
+
+
+
+
+
+int main(){
+  vector<int> arr = {64, 25, 12, 22, 11};
+  mergeSort(arr, 0, arr.size() - 1);
+
+  for (auto value : arr)
+  {
+    cout << value << " ";
+  }
+  return 0;
+}
